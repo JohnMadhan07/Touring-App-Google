c9597fd946f469a9fb3a7d9835b1e5c39af3c999
Madhan <madhan12n@gmail.com>
2024-01-12 23:51:25 +0000

    Add Card element for styling

--- src/user/pages/UsersItem.js
+++ src/user/pages/UsersItem.js
@@ -1,22 +1,26 @@
 import React from "react";
 import Avatar from "../../shared/components/UIElements/Avatar";
+import { Link } from "react-router-dom";
+import Card from "../../shared/components/UIElements/Card";
 import "./UsersItem.css";
 
 const UsersItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placecount}
-            {props.placecount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places'`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placecount}
+              {props.placecount === 1 ? " Place" : " Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
