93b0c793105b5e1b99c46837f11a4ae30dde810b
Madhan <madhan12n@gmail.com>
2024-01-13 19:24:37 +0000

    Adding Header

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import './shared/components/Navigation/MainNavigation.css'
 
 const App = () => {
   return (
     <Router>
+      <MainNavigation/>
+      <main>
       <Switch>
         <Route path="/" exact>
           <Users />
@@ -21,6 +25,7 @@ const App = () => {
         </Route>
         <Redirect to="/" />
       </Switch>
+      </main>
     </Router>
   );
 };
