88e92152698a52b8ca390831c40d9729c16d8e79
Madhan <madhan12n@gmail.com>
2024-04-05 09:52:31 +0100

    Display an User in UI

--- src/user/pages/UsersItem.js
+++ src/user/pages/UsersItem.js
@@ -8,16 +8,16 @@ const UsersItem = (props) => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
-        <Link to={`/${props.id}/places'`}>
+        <Link to={`/${props.id}/place`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
           </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
-            <h3>
+            {/* <h3>
               {props.placecount}
               {props.placecount === 1 ? " Place" : " Places"}
-            </h3>
+            </h3> */}
           </div>
         </Link>
       </Card>
